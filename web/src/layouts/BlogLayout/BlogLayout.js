68be0cfc6539868729d6a30cb9aa0183a4f37411
Steven Sajja <stevensajja@gmail.com>
2020-11-14 16:23:40 +0200

    Authentication

--- web/src/layouts/BlogLayout/BlogLayout.js
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -1,6 +1,8 @@
 import { Link, routes } from '@redwoodjs/router'
+import { useAuth } from '@redwoodjs/auth'
 
 const BlogLayout = ({ children }) => {
+  const { logIn, logOut, isAuthenticated, currentUser } = useAuth()
   return (
     <>
       <h1>
@@ -14,6 +16,12 @@ const BlogLayout = ({ children }) => {
           <li>
             <Link to={routes.contact()}>Contact</Link>
           </li>
+          <li>
+            <button onClick={isAuthenticated ? logOut : logIn}>
+              {isAuthenticated ? 'Log Out' : 'Log In'}
+            </button>
+          </li>
+          {isAuthenticated && <li>{currentUser.email}</li>}
         </ul>
       </nav>
       <main>{children}</main>
